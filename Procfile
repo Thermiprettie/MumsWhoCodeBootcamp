037176f1367dfd30ed80d523f6f489c9f2adf76d
Sarah <sta8@student.le.ac.uk>
2022-05-17 01:07:26 +0100

    First commit after initialising

---
+++ Procfile
@@ -0,0 +1,2 @@
+#this link to our wsgi file
+web:gunicorn precise_weather.wsgi --log-file -
---
+++ WeatherApp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+import json  # import json to load json data to python dictionary
+import urllib.request  # urllib.request to make a request to api
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        # source contain JSON data from API
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='
+                                        + city + '&appid=92324efba4a381bff3ccb50abb8fcf82').read()
+
+        # converting JSON data to a dictionary
+        list_of_data = json.loads(source)
+
+        # data for variable list_of_data
+        data = {
+            'city': city,
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' '
+                        + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + 'k',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data ={}#empty dictionary. i.e. to return nothing
+        
+    # send dictionary to the home.html
+    return render(request, "index.html", data)
