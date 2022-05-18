f32f29038a0235d73b398092cc749bfc257c8f4d
Sarah <sta8@student.le.ac.uk>
2022-05-19 00:56:48 +0100

    added units of measurements

--- WeatherApp/views.py
+++ WeatherApp/views.py
@@ -22,8 +22,8 @@ def index(request):
             "coordinate": str(list_of_data['coord']['lon']) + ' '
                         + str(list_of_data['coord']['lat']),
             "temp": str(list_of_data['main']['temp']) + '°C',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
+            "pressure": str(list_of_data['main']['pressure']) + 'hPa',
+            "humidity": str(list_of_data['main']['humidity']) + '%',
             'main': str(list_of_data['weather'][0]['main']),
             'description': str(list_of_data['weather'][0]['description']),
             'icon': list_of_data['weather'][0]['icon'],
